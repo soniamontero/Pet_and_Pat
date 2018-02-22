0e717baa61720b2ac78f63465dc02be68ae14705
Sonia Montero <sonia.montero@skema.edu>
2018-02-22 18:09:59 +0800

    Pets Controllers fully working with the full CRUD. THX Bye

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,12 +1,54 @@
 class PetsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-  def new
+  def index
+    @pets = Pet.all
   end
 
-  def edit
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+    pet.user = current_user
+    if pet.save
+      redirect_to pet_path(pet)
+    else
+    end
   end
 
   def show
+    @pet = Pet.find(params[:id])
   end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
+  def destroy
+    pet = Pet.find(params[:id])
+    pet.user = current_user
+    if pet.destroy
+      redirect_to pets_path
+    else
+    end
+  end
+
+    #   @dose = Dose.find(params[:id])
+    # @cocktail = @dose.cocktail
+    # @dose.destroy
+    # redirect_to cocktail_path(@cocktail)
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :location, :description, :photo, :category)
+  end
+
 end
