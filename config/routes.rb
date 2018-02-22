544b033779e33dceb0afad11a466b049536da30f
Rahel Weiss <mail@rahelweiss.com>
2018-02-22 14:54:27 +0800

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#index'
+  root to: 'pages#home'
 
+
+    resources :pets do
+      resources :bookings, only: [:edit, :update, :new, :create]
+      end
+
+      resources :bookings, only: [:show, :destroy] do
+        resources :reviews, only: [:new, :create]
+      end
+
+      resources :dashboard, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
