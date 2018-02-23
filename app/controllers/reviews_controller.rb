a605ee9f5eec5b11a56224afb802f52b5421364e
Kate Catlin <kcatlin2@gmail.com>
2018-02-23 12:26:38 +0800

    Adding reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,26 @@
 class ReviewsController < ApplicationController
   def new
+    @booking = Booking.find(params[:booking_id])
     @review = Review.new
   end
+
+  def create
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @review.content = params[:review][:content]
+    @review.rating = params[:review][:rating]
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+
+class Review < ApplicationRecord
+  belongs_to :booking
+  validates :rating, presence: true, numericality: true, inclusion: { in: (0..5) }
+
 end
