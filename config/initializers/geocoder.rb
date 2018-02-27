a12b186253f799a2f837cc61cc3f4ff002d97df3
Sonia Montero <sonia.montero@skema.edu>
2018-02-27 10:57:36 +0800

    added the google api server key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,6 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :google,            # name of geocoding service (symbol)
   # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
@@ -17,6 +16,9 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
+  units: :km,                 # :km for kilometers or :mi for miles
+  lookup: :google,            # name of geocoding service (symbol)
+  api_key: ENV['GOOGLE_API_SERVER_KEY'],
+  user_https: true,
 )
