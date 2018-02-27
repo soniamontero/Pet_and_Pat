89d277258bee9976adf899d679f7666684d1de66
Kate Catlin <kcatlin2@gmail.com>
2018-02-27 12:56:38 +0800

    Got new section of pets needing review working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,23 @@
 class DashboardController < ApplicationController
   def show
+    @upcoming_bookings = Booking.where(user_id: current_user.id).where("end_date > ?",  Date.today)
+    @bookings_needing_review = get_bookings_needing_review
     @mypets = Pet.where(user: current_user)
-    @mypetsbooking = Booking.where()
+  end
+
+  def get_bookings_needing_review
+    all_bookings = Booking.where(user_id: current_user.id)
+    bookings_needing_review = []
+    past_bookings = all_bookings.where("end_date < ?",  Date.today)
+    reviews = Review.all
+    past_bookings.each do |booking|
+      reviews.each do |review|
+        if !review.booking_id == booking.id
+          binding.pry
+          bookings_needing_review << booking
+        end
+      end
+    end
+    return bookings_needing_review
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   def new
     @booking = Booking.find(params[:booking_id])
     @review = Review.new
+    binding.pry
   end
 
   def create
